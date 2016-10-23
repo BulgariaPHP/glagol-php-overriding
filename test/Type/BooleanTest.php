4c02520704f040bf6e8f6c9471149f05c26ecc32
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2016-10-23 17:56:41 +0200

    Added tests for boolean type

---
+++ test/Type/BooleanTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+namespace GlagolTest\Overriding\Type;
+
+use Glagol\Overriding\Type\Boolean;
+use PHPUnit_Framework_TestCase;
+
+class BooleanTest extends PHPUnit_Framework_TestCase
+{
+    public function testShouldIdentifyTrueAsBoolean()
+    {
+        $this->assertTrue((new Boolean())->isSameTypeAs(true));
+    }
+
+    public function testShouldIdentifyFalseAsBoolean()
+    {
+        $this->assertTrue((new Boolean())->isSameTypeAs(false));
+    }
+
+    public function testShouldNotIdentifyStringAsBoolean()
+    {
+        $this->assertFalse((new Boolean())->isSameTypeAs("string"));
+    }
+
+    public function testShouldNotIdentifyIntAsBoolean()
+    {
+        $this->assertFalse((new Boolean())->isSameTypeAs(234));
+    }
+
+    public function testShouldNotIdentifyFloatAsBoolean()
+    {
+        $this->assertFalse((new Boolean())->isSameTypeAs(234.234));
+    }
+}
