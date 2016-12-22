356c5722fc54e8c816899606d67810e9cd66d5a5
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2016-12-22 15:27:38 +0100

    Testing execution of empty signature rule

--- test/OverriderTest.php
+++ test/OverriderTest.php
@@ -123,7 +123,6 @@ class OverriderTest extends PHPUnit_Framework_TestCase
         $overrider = new Overrider();
 
         $this->assertInstanceOf(Rule::class, $overrider->override(function (int $a) {}, new Integer()));
-        $this->assertInstanceOf(Overrider::class, $overrider->execute(2));
     }
 
     public function testShouldExecuteOnlyOnWhenCondition()
@@ -164,6 +163,29 @@ class OverriderTest extends PHPUnit_Framework_TestCase
         }, new Str(), new Real());
 
         $overrider->execute("abv", 2.3, 123);
+    }
+
+    public function testExecuteARuleWithEmptySignature()
+    {
+        $this->expectException(CannotMatchConstructorException::class);
+
+        $overrider = new Overrider();
+
+        $overrider->override(function () {
+            return true;
+        });
+
+        $overrider->override(function (int $a) {
+            return false;
+        }, new Integer());
+
+        $overrider->override(function (string $a) {
+            return false;
+        }, new Str());
+
+        $this->assertTrue($overrider->execute());
+        $this->assertFalse($overrider->execute(123));
+        $this->assertFalse($overrider->execute('123'));
     }
 
     public function testReturnValueShouldBeFromExecutedRule()
